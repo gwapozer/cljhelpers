45d044eac8228c66ca31826b06102f36638226ba
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-22 15:49:28 -0800

    Update eval_obj.clj

--- cljhelpers/src/cljhelpers/eval_obj.clj
+++ cljhelpers/src/cljhelpers/eval_obj.clj
@@ -1,5 +1,15 @@
 (ns cljhelpers.eval-obj)
 
+(defn get-set [s val]
+  (if (> (count (rest s)) 0)
+    (do
+      (def output (concat val (first s)))
+      (get-set (rest s) output)
+      )
+    (concat val (first s))
+    )
+  )
+
 (defn iterate-obj [obj-list msg]
   (if (> (count (rest obj-list)) 0)
     (do
@@ -9,5 +19,3 @@
     (str msg (prn-str (first obj-list)))
     )
   )
-
-(def test-var (iterate-obj ['(6 4 4) (+ 3 9 4)] nil))
