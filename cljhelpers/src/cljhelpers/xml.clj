629fe1ef0cb157cca703476d7aec9fd8af4f0122
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-22 15:51:10 -0800

    Update xml.clj

--- cljhelpers/src/cljhelpers/xml.clj
+++ cljhelpers/src/cljhelpers/xml.clj
@@ -7,7 +7,7 @@
 (def node (atom {:name nil :value :nil}))
 (def xml-list {:node node :child-list (atom '(node))})
 
-(def xml-string "<root><parent><child></child></parent></root>")
+(def xml-string "<root>data<parent description=\"test\"><child>inner data</child></parent></root>")
 
 (defn xml-reader [xmlstr]
 
@@ -30,25 +30,39 @@
     )
   )
 
-(defn xml-obj [s head tail]
-  (if (> (count (rest s)) 0)
+(defn is-stringify [bool-val chr]
+  (if (and (= bool-val false) (= chr (first "\"")))
+    true
+    (if (and (= bool-val true) (= chr (first "\"")))
+      false
+      bool-val)
+    ))
+
+(defn xml-obj [str head tail stringify elt elt-data]
+  (if (> (count (rest str)) 0)
     (do
-      (let [chr (first s)
-            tail (= chr (first ">"))
-            elt (and (= head true ) (= tail true))
-            head (or (and (= tail false) (= head true))(and (= tail false) (= head false) (= chr (first "<")))  )
+      (let [chr (first str)
+            stringify (is-stringify stringify chr)
+            head (if (and (= stringify false) (= head false) (= chr (first "<"))) true (cond (= elt true) false :else head))
+            tail (if (and (= stringify false) (= tail false) (= chr (first ">"))) true (cond (= elt true) false :else tail))
+            elt (if (and (= head true ) (= tail true) (= elt false)) true (if (and (= head true ) (= tail false)) false elt ) )
+            ;elt-data (cond (= elt true) ("sdf") :else elt-data)
             depth 1
             ;cntstring (= stringify (+ cntstring 1))
             ]
-        (println "chr:" chr " head: " head " tail: " tail)
-        (xml-obj (rest s) head tail)
+        (println "chr:" chr " head: " head " tail: " tail " is-stringify:" stringify " Elt data: " elt-data)
+        (xml-obj (rest str) head tail stringify elt elt-data)
         )
       )
-    (println (first s))
+    (println (first str))
     )
   )
 
-(iterate-obj xml-string)
-(xml-obj xml-string true true)
+;(iterate-obj xml-string)
+(xml-obj xml-string false false false false nil)
 ;(xml-reader xml-string)
 
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Welcome to clj helpers!"))
