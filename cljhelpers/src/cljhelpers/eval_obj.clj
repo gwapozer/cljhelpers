3341830b9eea855b81ddb6dbc6e5ddf036188dbd
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-23 12:44:24 -0800

    Update eval_obj.clj

--- cljhelpers/src/cljhelpers/eval_obj.clj
+++ cljhelpers/src/cljhelpers/eval_obj.clj
@@ -1,15 +1,5 @@
 (ns cljhelpers.eval-obj)
 
-(defn get-set [s val]
-  (if (> (count (rest s)) 0)
-    (do
-      (def output (concat val (first s)))
-      (get-set (rest s) output)
-      )
-    (concat val (first s))
-    )
-  )
-
 (defn iterate-obj [obj-list msg]
   (if (> (count (rest obj-list)) 0)
     (do
@@ -17,5 +7,25 @@
       (iterate-obj (rest obj-list) output)
       )
     (str msg (prn-str (first obj-list)))
+    )
+  )
+
+(defn filter-obj [filters data]
+  (if (> (count (rest filters)) 0)
+    (do
+      (def filtered-data (filter (every-pred (first filters)) data))
+      (filter-obj (rest filters) filtered-data)
+      )
+    (filter (every-pred (first filters)) data)
+    )
+  )
+
+(defn sort-obj [comparators data]
+  (if (> (count (rest comparators)) 0)
+    (do
+      (def sorted-data (sort (first comparators) data))
+      (sort-obj (rest comparators) sorted-data)
+      )
+    (sort (first comparators) data)
     )
   )
