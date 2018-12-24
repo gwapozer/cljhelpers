474778c79b37b018911f72bf38f91f94e97999e1
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-23 22:23:53 -0800

    Update eval_obj_test.clj

--- cljhelpers/test/cljhelpers/eval_obj_test.clj
+++ cljhelpers/test/cljhelpers/eval_obj_test.clj
@@ -7,8 +7,12 @@
   (def data
     [{:id 1 :first-name "John1" :last-name "Doe1" :age "14"}
      {:id 4 :first-name "John2" :last-name "Doe2" :age "54"}
-     {:id 4 :first-name "John3" :last-name "Doe3" :age "34"}
-     {:id 4 :first-name "John4" :last-name "Doe4" :age "12"}
+     ;{:id 4 :first-name "John3" :last-name "Doe3" :age "34" :education [{:gpa 3} {:gpa 5}] }
+     ;{:id 4 :first-name "John4" :last-name "Doe4" :age "12" :education [{:gpa 1} {:gpa 2}]}
+     {:id 4 :first-name "John5" :last-name "Doe5" :age "34" :education {:gpa 1 :school "USV"} }
+     {:id 6 :first-name "John3" :last-name "Doe3" :age "34" :education {:gpa 5 :school "USV"} }
+     {:id 4 :first-name "John4" :last-name "Doe4" :age "12" :job [{:title "SW"} {:title "data parser"}]}
+
      {:id 5 :first-name "John5" :last-name "Doe5" :age "24"}])
 
   (def test0 (eo/filter-obj [(comp #(> %1 3) :id)] data))
@@ -19,8 +23,14 @@
   (def test4 (eo/sort-obj [(fn[x y] (if (< (:id x) (:id y) ) true false))] data))
   (def test5 (eo/sort-obj [(fn[x y] (compare (:id y) (:id x)))] data))
   (def test6 (eo/sort-obj [(fn [x y] (compare [(:id y) (:first-name x)] [(:id x) (:first-name y)]))] data))
+  (def test7 (eo/update-obj [[[:id] #(+ % 3)]] data))
+  (def test8 (eo/map-obj [(fn [x] (:education x))] data))
+  (def test9 (eo/filter-obj [(fn [x] (not= x nil))] test8))
+  (def test0 (eo/filter-obj [(comp #(vector? %) :education)] data))
 
-  (println test6)
-
+  (def test10 (eo/filter-obj [(comp #(if-not (nil? %) true false) :education)] data))
+  (def test10 (eo/update-obj [[[:education :gpa] #(+ % 3)]] (into [] test10 )))
+  (println test10)
   )
+
 (test-filter-sort)
