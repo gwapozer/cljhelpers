63290b3690bbc7ec4803428a5acc370037b67d9c
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-26 23:07:51 -0800

    Update xml.clj

--- cljhelpers/src/cljhelpers/xml.clj
+++ cljhelpers/src/cljhelpers/xml.clj
@@ -8,6 +8,7 @@
 (def xml-list {:node node :child-list (atom '(node))})
 
 (def xml-string "<root>data<parent description=\"test\"><child>inner data</child></parent></root>")
+(def simple-xml-string "<root><child>inner data</child></root>")
 
 (defn xml-reader [xmlstr]
 
@@ -38,28 +39,41 @@
       bool-val)
     ))
 
-(defn xml-obj [str head tail stringify elt elt-data]
-  (if (> (count (rest str)) 0)
+(defn xml-obj [str-data node-list curr-node head tail stringify elt-end elt-name elt-data]
+  (if (> (count (rest str-data)) 0)
     (do
-      (let [chr (first str)
+      (let [chr (first str-data)
             stringify (is-stringify stringify chr)
-            head (if (and (= stringify false) (= head false) (= chr (first "<"))) true (cond (= elt true) false :else head))
-            tail (if (and (= stringify false) (= tail false) (= chr (first ">"))) true (cond (= elt true) false :else tail))
-            elt (if (and (= head true ) (= tail true) (= elt false)) true (if (and (= head true ) (= tail false)) false elt ) )
-            ;elt-data (cond (= elt true) ("sdf") :else elt-data)
-            depth 1
-            ;cntstring (= stringify (+ cntstring 1))
+            head (if (and (= stringify false) (= chr (first "<"))) true (cond (= tail true) false :else head))
+            tail (if (and (= stringify false) (= chr (first ">"))) true (cond (= head true) false :else tail))
+            elt-start (if (and (= stringify false) (= chr (first "<"))) true false)
+            elt-end (if (and (= stringify false) (= chr (first "/"))) true (cond (= head true) false :else elt-end))
             ]
-        (println "chr:" chr " head: " head " tail: " tail " is-stringify:" stringify " Elt data: " elt-data)
-        (xml-obj (rest str) head tail stringify elt elt-data)
+
+        (if (= elt-start true)
+          (do
+            (println curr-node)
+            (def curr-node {:name nil :data nil :child-list nil})
+            )
+          curr-node)
+
+        (def -name (if (and (= elt-start false) (= elt-end false) (= head true ) (= tail false) (= stringify false)) (str elt-name chr) (cond (= elt-start true) "" :else elt-name)))
+        (def -data (if (= tail true) (str elt-data chr) ""))
+
+        (def updated-node (assoc curr-node :name -name :data -data))
+
+        ;(println "chr:" chr " head: " head " tail: " tail " is-stringify:" stringify " Elt start: " elt-start " Elt End: " elt-end " Elt name: " -name " Elt data: " -data)
+        ;(println "Curr node: " updated-node)
+
+        (xml-obj (rest str-data) node-list updated-node head tail stringify elt-end -name -data)
         )
       )
-    (println (first str))
+    (println (first str-data))
     )
   )
 
 ;(iterate-obj xml-string)
-(xml-obj xml-string false false false false nil)
+(xml-obj simple-xml-string {} {} false false false false "" "")
 ;(xml-reader xml-string)
 
 (defn -main
