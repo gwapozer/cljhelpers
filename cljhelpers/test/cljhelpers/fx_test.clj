90e71e5602781164407e1b076a6d0a9ec21f8959
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-30 20:43:51 -0800

    Add files via upload

--- cljhelpers/test/cljhelpers/fx_test.clj
+++ cljhelpers/test/cljhelpers/fx_test.clj
@@ -3,6 +3,8 @@
             [cljhelpers.fx_obj :as fo]
             [widget.evalobj-gui :as eg]
             [cljhelpers.eval-obj :as eo]
+            [entity.id-name :refer :all]
+            [cljhelpers.list_helpers :refer :all]
             )
   (:import (javafx.scene SceneBuilder)
            (javafx.scene.control ButtonBuilder)
@@ -10,7 +12,9 @@
            (javafx.stage StageBuilder)
            (javafx.fxml FXMLLoader)
            (javas fxhelpers)
-           )
+           (javafx.event EventHandler ActionEvent)
+           (javafx.collections FXCollections)
+           (javafx.scene.control.cell MapValueFactory PropertyValueFactory))
   (:use cljhelpers.FxRun)
   )
 
@@ -20,21 +24,6 @@
 (def loader  (FXMLLoader/load loc))
 
 (run-now
-  ;(def scene  (scene (.. SceneBuilder create (height 480) (width 640)
-  ;                       (root loader) build)))
-  ;(def stagebuilder (.. StageBuilder create (title "Hello JavaFX") build))
-  ;(.setScene stagebuilder scene build)
-
-  ;(def stage-data
-  ;  (.. StageBuilder create
-  ;      (title "Hello JavaFX")
-  ;      (scene (.. SceneBuilder create
-  ;                 (height 480) (width 640)
-  ;                 (root loader)
-  ;                 build))
-  ;      build)
-  ;  )
-
   (reset! stage
           (.. StageBuilder create
               (title "Hello JavaFX")
@@ -46,17 +35,46 @@
           )
   )
 
+(defn settext []
+  ;(run-later (-> (fo/fxnode :txtinfo) (.setText "add data data")))
+  (-> (fo/fxnode :txtinfo) (.setText "add data data"))
+  (-> (fo/fxnode :ddlid-name) (.setValue "data2"))
+  )
+
+(defn comboselect []
+
+  (def select-val (-> (fo/fxnode :ddlid-name) (.getValue)))
+  (-> (fo/fxnode :txtinfo) (.setText select-val))
+  ;(run-later (-> (fo/fxnode :txtinfo) (.setText select-val)))
+  )
+
 (defn load-gui []
 
   (fo/set-atom-stage @stage)
   (run-now (.show @stage))
-  (run-now (-> (fo/fxnode :txtinfo) (.setText  "some data")) )
+  (run-now (-> (fo/fxnode :txtinfo) (.setText "some data")))
+
+  (def idname-data (FXCollections/observableArrayList '((id-name 1 "data1") (id-name 2 "data2"))))
+  (def combo-data (FXCollections/observableArrayList '("data1" "data2" "data3")))
+  (def combo-mapdata (FXCollections/observableMap {:id 1 :name "test"}))
+
+  (run-now (LoadCmb (fo/fxnode :ddlid-name) combo-data))
+  (run-now (-> (fo/fxnode :ddlid-name) (.setValue "data1")))
+
+  (run-now (-> (fo/fxnode :tblvinfo) (.setCellValueFactory (PropertyValueFactory. (String.)))))
+
+  ;(run-now (-> (fo/fxnode :tblvinfo) (.setCellValueFactory (MapValueFactory. (str)))))
+  (run-now (-> (fo/fxnode :tblvinfo) (.setItems combo-data)))
+
+  (run-now (-> (fo/fxnode :btnrun) (.setOnAction (proxy [EventHandler] [] (handle [^ActionEvent event] (settext))))))
+  (run-now (-> (fo/fxnode :ddlid-name) (.setOnAction (proxy [EventHandler] [] (handle [^ActionEvent event] (comboselect))))))
+
   ;(run-now (.show @stage))
   ;(reset! (fo/atom-stage) 5)
 
-  ;(def msg (eo/iterate-obj [(fo/fxnode :txtinfo) ] ""))
+  ;(def getdata (run-now (-> (fo/fxnode :ddlid-name) (.getItems) (.add "sdfsf"))))
+  ;(def msg (eo/iterate-obj [ getdata ] ""))
   ;(run-now (eg/LoadEvalMessage msg))
-
   )
 
 (load-gui)
