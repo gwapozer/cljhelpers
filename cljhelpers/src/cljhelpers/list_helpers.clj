9fbe6f5089e0bc1e2faf0513e4096e6dd6cb7a03
gwapozer <45476238+gwapozer@users.noreply.github.com>
2018-12-31 20:00:55 -0800

    Add files via upload

--- cljhelpers/src/cljhelpers/list_helpers.clj
+++ cljhelpers/src/cljhelpers/list_helpers.clj
@@ -1,5 +1,22 @@
-(ns cljhelpers.list_helpers)
+(ns cljhelpers.list_helpers
+  (:import (javafx.scene.control.cell MapValueFactory)))
 
-(defn LoadCmb [cmbnode list]
-  (-> cmbnode (.setItems list))
+(defn LoadCmb [cmbnode datas]
+  (-> cmbnode (.setItems datas))
+  )
+
+(defn LoadTblvw [tblvwnode datas key-names]
+  (-> tblvwnode (.setItems datas))
+
+  ;TODO simplify by doing a map
+  (def tablecols (-> tblvwnode (.getColumns)))
+  (loop [i 0]
+    (if (< i (count tablecols))
+      (do
+        (def tblcol (nth tablecols i))
+        (-> tblcol (.setCellValueFactory (MapValueFactory. (some #{(.getId tblcol)} key-names))))
+        (recur (inc i))
+        )))
+  ;(map #(-> % (.setCellValueFactory (MapValueFactory. (some #{(.getId %)} key-names)))) tablecols)
+  ;END
   )
