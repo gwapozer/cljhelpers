cfbeb962e9fec345b449fa8ccbfce4e401daf82f
gwapozer <45476238+gwapozer@users.noreply.github.com>
2019-01-13 19:09:37 -0800

    Update person_definition.clj

--- cljhelpers/src/definition/person_definition.clj
+++ cljhelpers/src/definition/person_definition.clj
@@ -3,8 +3,8 @@
   (:import (definition.table_struct table-struct ))
   )
 
-(def person_definition #{(table-struct. 1 "ID" "ID" "INTEGER" 10 true false)
-                         (table-struct. 2 "FirstName" "FirstName" "VARCHAR" 50 false false)
-                         (table-struct. 3 "LastName" "LastName" "VARCHAR" 50  false false)
-                         (table-struct. 3 "Email" "Email" "VARCHAR" 150 false false)
+(def person_definition #{(table-struct. 1 "ID" "ID" "INTEGER" 10 true false true)
+                         (table-struct. 2 "FirstName" "FirstName" "VARCHAR" 50 false false false)
+                         (table-struct. 3 "LastName" "LastName" "VARCHAR" 50  false false false)
+                         (table-struct. 4 "Email" "Email" "VARCHAR" 150 false false false)
                          })
